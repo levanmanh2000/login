8d5a57ea94894d0bb9378c44571dda374e81822c
levanmanh2000 <78593304+levanmanh2000@users.noreply.github.com>
2021-02-26 21:57:59 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, Image, TextInput, StyleSheet, TouchableOpacity, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hello-world: ltmt5-nhom 7</Text>
-      <StatusBar style="auto" />
+function Login({ navigation }) {
+  return(
+    <View style={styles.all} >
+        
+        
+
+        <TextInput style={styles.input} 
+            placeholder='tai khoan' ></TextInput>
+
+        <TextInput style={styles.input} 
+            placeholder='mat khau' 
+            secureTextEntry ></TextInput>
+
+        <TouchableOpacity style={styles.btLogin} 
+            title="LOGIN"
+            onPress={() => navigation.navigate('Proo')} >
+            <Text style={styles.textLogin} >LOGIN</Text>
+        </TouchableOpacity>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+function Proo({ navigation }) {
+  return (
+      <View style={styles.pr} >
+          <Text>Profession</Text>
+          <Button title="Go back" onPress={() => navigation.goBack()} />
+      </View>
+  );
+}
+
+const styles=StyleSheet.create({
+  all:{
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center',
+      backgroundColor:'#fff'
   },
+  img:{
+      width:300,
+      height:150,
+      resizeMode:'contain'
+  },
+  input:{
+      width:300,
+      height:40,
+      backgroundColor:'#fff',
+      borderRadius:5,
+      fontSize:18,
+      paddingHorizontal:15
+  },
+  btLogin:{
+      width:300,
+      height:50,
+      backgroundColor:'#000',
+      borderRadius:5,
+      justifyContent:'center',
+      alignItems:'center',
+      marginTop:40
+  },
+  textLogin:{
+      color:'#fff',
+      fontSize:18,
+      fontWeight:'bold'
+  },
+  pr:{
+    flex:1,
+    fontSize:20,
+    justifyContent:'center',
+    alignItems:'center'
+  }
 });
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown:false }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Proo" component={Proo} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
